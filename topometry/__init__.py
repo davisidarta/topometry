5dc0440b8828658025cbed7421ca1f5efab54193
davisidarta <44589562+davisidarta@users.noreply.github.com>
2021-04-14 17:45:09 -0300

    Fix for installation .1

--- topometry/__init__.py
+++ topometry/__init__.py
@@ -3,7 +3,7 @@ from topometry.tpgraph import diffusion as dm
 from topometry.tpgraph import cknn as ck
 from topometry.spectral import spectral as spt
 from topometry.layouts import net, uni, pac, tri, mde
-from topometry import plot as pl
-from topometry import models as ml
+from topometry.plot import * as pl
+import topometry.models import * as ml
 
 from topometry.version import __version__
