3c3da8a9b92204e08ba5228729a7cb58180b7ebb
davisidarta <44589562+davisidarta@users.noreply.github.com>
2021-04-14 17:37:52 -0300

    14/4 dev .1

--- topometry/__init__.py
+++ topometry/__init__.py
@@ -4,6 +4,6 @@ from topometry.tpgraph import cknn as ck
 from topometry.spectral import spectral as spt
 from topometry.layouts import net, uni, pac, tri, mde
 from topometry import plot as pl
-import topometry.models as ml
+from topometry import models as ml
 
 from topometry.version import __version__
