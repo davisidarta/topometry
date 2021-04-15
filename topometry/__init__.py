b0f5c528c43f708a37f5831e65f2928c81f99e9f
davisidarta <davisidarta@gmail.com>
2021-04-14 21:03:03 -0300

    init fixes

--- topometry/__init__.py
+++ topometry/__init__.py
@@ -2,8 +2,8 @@ from topometry.base import ann as nn
 from topometry.tpgraph import diffusion as dm
 from topometry.tpgraph import cknn as ck
 from topometry.spectral import spectral as spt
-from topometry.layouts import net, uni, pac, tri, mde
-from topometry.plot import * as pl
-import topometry.models import * as ml
+from topometry.layouts import uni, pac, tri, mde
+from topometry.plot import *
+from topometry.models import *
 
 from topometry.version import __version__
