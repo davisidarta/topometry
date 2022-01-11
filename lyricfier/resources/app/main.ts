85c434e331909aef810bc89efaa544491284420c
davisidarta <davisidarta@gmail.com>
2022-01-10 22:21:58 -0300

    Clean up

--- lyricfier/resources/app/main.ts
+++
@@ -1,6 +0,0 @@
-import {Lyricfier} from "./Lyricfier";
-import electron = require("electron");
-import {Settings} from "./Settings";
-const app:Electron.App = electron.app;
-const settings = new Settings();
-const lyricfier = new Lyricfier(app, settings, __dirname);
