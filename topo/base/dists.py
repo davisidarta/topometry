c77a22db6805e515530e6e9c3f6f93a61c7c2664
davisidarta <davisidarta@gmail.com>
2022-03-10 01:09:06 -0300

    Pre-print release

--- topo/base/dists.py
+++ topo/base/dists.py
@@ -34,7 +34,11 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import numba
+try:
+    import numba
+except:
+    print('DEBUG: Numba not installed.')
+
 import numpy as np
 import scipy.stats
 from sklearn.metrics import pairwise_distances
