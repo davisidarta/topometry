46a399db343b7c161cb73421bae9af6919a0a677
davisidarta <davisidarta@gmail.com>
2022-07-15 15:13:32 -0300

    Correct typos

--- build/lib/topo/spectral/__init__.py
+++
@@ -1,2 +0,0 @@
-from ._spectral import graph_laplacian, LE, multicomponent_LE, _sparse_anisotropic_diffusion_symmetric
-from .umap_layouts import *
--- build/lib/topo/tpgraph/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cknn import *
-from .diffusion import *
-from .multiscale import multiscale, decay_plot
