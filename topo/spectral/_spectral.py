bea38b3e22beb31dfa8c79343f4d37b37a8b5e14
davisidarta <davisidarta@gmail.com>
2022-09-04 01:58:13 -0300

    Refactor classes

--- topo/spectral/_spectral.py
+++ topo/spectral/_spectral.py
@@ -254,7 +254,7 @@ def LE(W, n_eigs=10, laplacian_type='random_walk', drop_first=True, weight=True,
             evals, evecs = sparse.linalg.eigsh(
                 L, k=n_eigs, which='SM', tol=eigen_tol, ncv=num_lanczos_vectors, maxiter=L.shape[0] * 5)
         else:
-            eigenvalues, eigenvectors = sparse.linalg.lobpcg(
+            evals, evecs = sparse.linalg.lobpcg(
                 L, random_state.normal(size=(L.shape[0], n_eigs)), largest=False, tol=1e-8
             ) 
     except sparse.linalg.ArpackError:
@@ -762,26 +762,4 @@ def spectral_clustering(init, max_svd_restarts=30, n_iter_max=30, random_state=N
     return labels
 
 
-def _init_arpack_v0(size, random_state):
-    """Initialize the starting vector for iteration in ARPACK functions.
-    Initialize a ndarray with values sampled from the uniform distribution on
-    [-1, 1]. This initialization model has been chosen to be consistent with
-    the ARPACK one as another initialization can lead to convergence issues.
-    Parameters
-    ----------
-    size : int
-        The size of the eigenvalue vector to be initialized.
-    random_state : int, RandomState instance or None, default=None
-        The seed of the pseudo random number generator used to generate a
-        uniform distribution. If int, random_state is the seed used by the
-        random number generator; If RandomState instance, random_state is the
-        random number generator; If None, the random number generator is the
-        RandomState instance used by `np.random`.
-    Returns
-    -------
-    v0 : ndarray of shape (size,)
-        The initialized vector.
-    """
-    random_state = check_random_state(random_state)
-    v0 = random_state.uniform(-1, 1, size)
-    return
+
